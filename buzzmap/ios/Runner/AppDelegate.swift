1dbf667c9be6044578fa3d75998e18a4adc5f000
Tyrel Cruz <tyrelcruz@Tyrels-MacBook-Pro.local>
2025-04-18 00:22:30 +0800

    Firebase setup || Androind & iOS

--- buzzmap/ios/Runner/AppDelegate.swift
+++ buzzmap/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import Firebase
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,12 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Initialize Firebase
+    FirebaseApp.configure()
+
+    // Ensure the Flutter engine is set up
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
